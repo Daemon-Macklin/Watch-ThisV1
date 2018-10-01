e24b0750c3d274ffb4d144562b6e3bef04a6b8af
Daemon-Macklin <daemon.macklin@outlook.com>
2018-10-01 16:19:20 +0100

    Initial Git Commit

---
+++ models/movies.js
@@ -0,0 +1,7 @@
+const movies = [
+    {id : 1, type : "Movie", title : "Finding Nemo", genre: "Animation", upvotes : "0" },
+    {id : 2, type : "Movie", title : "Dark Knight", genre: "Action", upvotes : "0" },
+    {id : 3, type : "Movie", title : "Silence of the lambs", genre: "Animation", upvotes : "0" }
+];
+
+module.exports = movies;
---
+++ routes/movies.js
@@ -0,0 +1,61 @@
+let movies = require('../models/movies');
+let express = require('express');
+let router = express.Router();
+
+router.findAll = (req, res) => {
+    // Return a JSON representation of our list
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(movies,null,5));
+    res.json(movies);
+};
+
+router.findOne = (req, res) => {
+const movie = getByValue(movies, req.params.id);
+// Create a donation variable and use the helper function
+// to find
+// req.params.id
+// in our donations array
+if(movie!=null){
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(movie,null,5));
+    res.json(movie);
+}else{
+    res.json('Movie not found');
+}
+// Then either return the found donation
+// or a suitable error message
+};
+
+router.addMovie = (req, res) => {
+    //Add a new donation to our list
+    var id = Math.floor((Math.random() * 1000000) + 1); //Randomly generate an id
+    // parameters to store
+    // id (for id)
+    // req.body.paymenttype (for paymenttype)
+    // req.body.amount (for amount)
+    // 0 (for upvotes)
+    const movie = {id : id, type : req.body.type, title: req.body.title, genre: req.body.genre,  upvotes: 0};
+    var currentSize = movies.length;
+
+    movies.push(movie);
+
+    if((currentSize + 1) == movies.length)
+        res.json({ message: 'Movie Added!'});
+    else
+        res.json({ message: 'Movie NOT Added!'});
+};
+
+router.incrementUpvotes = (req, res) => {
+    // Find the relevant donation based on params id passed in
+    // Add 1 to upvotes property of the selected donation based on its id
+    var movie = getByValue(movies,req.params.id);
+    movie.upvotes += 1;
+};
+
+function getByValue(array, id) {
+    var result  = array.filter(function(obj){return obj.id == id;} );
+    return result ? result[0] : null; // or undefined
+}
+
+
+module.exports = router;
