6bf1146fdbb2682e02b593f4a6895b04ac041754
Daemon Macklin <daemon.macklin@outlook.com>
2018-11-09 17:50:53 +0000

    Fixing set headers bug.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const media = require('./routes/media');
+const cors = require('cors');
 var app = express();
 
 
@@ -17,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
